75b9ae1a090e58f546bcc6c5f36a6b2ab35cd34a
karanprajap02-hash <karan.prajap02@gmail.com>
2026-07-15 22:23:24 +0530

    The second question is done

---
+++ question2.py
@@ -0,0 +1,8 @@
+print("----Check the number is even or odd----")
+num= int(input("Enter the number: "))
+
+if num%2==0:
+    print("\n\nThe number is even")
+else:
+    print("The number is odd")
+    
