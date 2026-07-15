c69cb235b93e42b0604ecc6cefaae643b24198af
karanprajap02-hash <karan.prajap02@gmail.com>
2026-07-15 22:12:23 +0530

    the first question is done

---
+++ question1.py
@@ -0,0 +1,11 @@
+print("Enter the two numbers for swap")
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+
+# there are two aprroch 1:with third variable
+# 2: by addition or sub solution
+print(f"The first value as a:{a} and second value as b:{b} \n")
+c=a
+a=b
+b=c
+print(f"The first value as a:{a} and second value as b:{b} \n")
